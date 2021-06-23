49e0bf2f15a9de860918e075ff3fa40dd3137c69
Modestas Sinkevičius <modestas.sinkevicius@visma.com>
2021-06-23 13:36:27 +0300

    added API controller, added httpclient

--- AnagramSolver/Program.cs
+++ AnagramSolver/Program.cs
@@ -3,16 +3,37 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using AnagramSolver.BusinessLogic;
 using AnagramSolver.Contracts;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AnagramSolver.Cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main()
+        {
+            Console.WriteLine("Connecting...");
+            //ContainerSetup(args);
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://localhost:44357/AnagramAPI/GetAnagram/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();               
+
+                Console.WriteLine(response.StatusCode);
+            }
+            catch (HttpRequestException e)
+            {   
+                Console.WriteLine($"\nMessage :{e.Message}");
+            }
+        }
+
+        static void ContainerSetup(string[] args)
         {
             using IHost host = CreateHostBuilder(args).Build();
 
-            Console.OutputEncoding = System.Text.Encoding.UTF8;                       
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             using IServiceScope serviceScope = host.Services.CreateScope();
             IServiceProvider provider = serviceScope.ServiceProvider;
@@ -20,9 +41,10 @@ namespace AnagramSolver.Cli
             ConsoleInterface ci = provider.GetRequiredService<ConsoleInterface>();
 
             ci.OutputResult();
-            
+
             host.Run();
         }
+
         static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((context, services) =>
