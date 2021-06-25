b3d64c8788de5d40513a8e07b8723746694d6c2a
Modestas Sinkevičius <modestas.sinkevicius@visma.com>
2021-06-25 15:36:27 +0300

    added availability download dictionary

---
+++ AnagramSolver.WebApp/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AnagramSolver.WebApp.Controllers
+{
+    public class FileController : Controller
+    {        
+        public async Task<IActionResult> DownloadDictionary()
+        {
+            var path = @"C:\DownloadableContent\zodynas.txt";
+            var memory = new MemoryStream();
+            using(FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                await fs.CopyToAsync(memory);
+            }
+            memory.Position = 0;           
+
+            return File(memory, "text/plain", "zodynas.txt");
+        }
+    }
+}
