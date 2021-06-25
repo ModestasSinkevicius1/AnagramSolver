4fc2ffd4240d5bea8a2fc6271b19401c051a22db
Modestas Sinkevičius <modestas.sinkevicius@visma.com>
2021-06-25 15:46:49 +0300

    changed back root path

--- AnagramSolver.WebApp/Startup.cs
+++ AnagramSolver.WebApp/Startup.cs
@@ -52,7 +52,7 @@ namespace AnagramSolver.WebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=FileAccess}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
