e364deaf55065714f4bc94aa78cbc617868fdcc3
Modestas Sinkevičius <modestas.sinkevicius@visma.com>
2021-07-08 08:39:24 +0300

    Revert "db context moved to constructor"
    This reverts commit adc74587d98be2a51baf3b5dfa6b9ae4cc0871f2.

--- AnagramSolver.BusinessLogic/DBWordRepositoryCodeFirst.cs
+++ AnagramSolver.BusinessLogic/DBWordRepositoryCodeFirst.cs
@@ -11,20 +11,18 @@ namespace AnagramSolver.BusinessLogic
 {
     public class DBWordRepositoryCodeFirst : IWordRepository
     {
-        private readonly AnagramDBCodeFirstContext db;
-        public DBWordRepositoryCodeFirst()
-        {
-            db = new AnagramDBCodeFirstContext();
-        }
 
         public IList<WordModel> GetWords()
         {
             IList<WordModel> words = new List<WordModel>();
-            
-            foreach (var word in db.Word.ToList())
+
+            using (var db = new AnagramDBCodeFirstContext())
             {
-                words.Add(new WordModel(word.ID, word.Word, word.Category));
-            }           
+                foreach (var word in db.Word.ToList())
+                {
+                    words.Add(new WordModel(word.ID, word.Word, word.Category));
+                }
+            }
 
             return words;
         }
@@ -32,15 +30,18 @@ namespace AnagramSolver.BusinessLogic
         public IList<WordModel> SearchWords(string myWord)
         {
             List<WordModel> words = new List<WordModel>();
-            
-            var wordsFound = from s in db.Word
-                                where s.Word.Contains(myWord)
-                                select s;
 
-            foreach (var word in wordsFound)
+            using (var db = new AnagramDBCodeFirstContext())
             {
-                words.Add(new WordModel(word.ID, word.Word, word.Category));
-            }         
+                var wordsFound = from s in db.Word
+                                 where s.Word.Contains(myWord)
+                                 select s;
+
+                foreach (var word in wordsFound)
+                {
+                    words.Add(new WordModel(word.ID, word.Word, word.Category));
+                }
+            }
 
             return words;
         }
@@ -54,20 +55,25 @@ namespace AnagramSolver.BusinessLogic
                     SearchingWord = myWord,
                     WordID = ana.Id,
                 };
-                
-                db.Add(cachedWord);                                 
+                using (var db = new AnagramDBCodeFirstContext())
+                {
+                    db.Add(cachedWord);
+                    db.SaveChanges();
+                }
             }
-            db.SaveChanges();
         }
 
         public bool CheckCachedWord(string myWord)
         {
             bool isWordExist = false;
             int count = 0;
-            
-            var myWordParam = new SqlParameter("myWord", myWord);
 
-            count = db.CachedWord.Count(t => t.SearchingWord == myWord);       
+            using (var db = new AnagramDBCodeFirstContext())
+            {
+                var myWordParam = new SqlParameter("myWord", myWord);
+
+                count = db.CachedWord.Count(t => t.SearchingWord == myWord);
+            }
 
             isWordExist = count > 0 ? true : false;
 
@@ -77,55 +83,67 @@ namespace AnagramSolver.BusinessLogic
         public IList<WordModel> GetWordFromCache(string myWord)
         {
             List<WordModel> anagrams = new();
-            
-            var query = from s in db.CachedWord
-                        join sa in db.Word on s.WordID equals sa.ID
-                        where s.SearchingWord == myWord
-                        select sa;
 
-            foreach (var word in query)
+            using (var db = new AnagramDBCodeFirstContext())
             {
-                anagrams.Add(new WordModel(word.ID, word.Word, word.Category));
+                var query = from s in db.CachedWord
+                            join sa in db.Word on s.WordID equals sa.ID
+                            where s.SearchingWord == myWord
+                            select sa;
+
+                foreach (var word in query)
+                {
+                    anagrams.Add(new WordModel(word.ID, word.Word, word.Category));
+                }
             }
-            
+
             return anagrams;
         }
 
         public void DeleteRecordFromWordTable(string myWord)
-        {           
-            var myWordParam = new SqlParameter("target", myWord);
-            db.Database.ExecuteSqlRaw("DeleteRecordFromWord @target", myWordParam);           
+        {
+            using (var db = new AnagramDBCodeFirstContext())
+            {
+                var myWordParam = new SqlParameter("target", myWord);
+                db.Database.ExecuteSqlRaw("DeleteRecordFromWord @target", myWordParam);
+            }
         }
 
         public void InsertUserLogToDB(IList<WordModel> words, string myWord, string userIp)
-        {            
-            foreach (WordModel ana in words)
+        {
+            using (var db = new AnagramDBCodeFirstContext())
             {
-                var userIpParam = new SqlParameter("@userIp", userIp);
-                var myWordParam = new SqlParameter("@searchingWord", myWord);
-                var datetimeParam = new SqlParameter("@searchTime", System.DateTime.Now.ToString());
-                var anagramIdParam = new SqlParameter("@foundAnagramId", ana.Id);
+                foreach (WordModel ana in words)
+                {
+                    var userIpParam = new SqlParameter("@userIp", userIp);
+                    var myWordParam = new SqlParameter("@searchingWord", myWord);
+                    var datetimeParam = new SqlParameter("@searchTime", System.DateTime.Now.ToString());
+                    var anagramIdParam = new SqlParameter("@foundAnagramId", ana.Id);
 
-                db.Database.ExecuteSqlRaw("InsertUserLog @userIp, @searchingWord, " +
-                    "@searchTime, @foundAnagramId",
-                    userIpParam, myWordParam, datetimeParam, anagramIdParam);
-            }           
+                    db.Database.ExecuteSqlRaw("InsertUserLog @userIp, @searchingWord, " +
+                        "@searchTime, @foundAnagramId",
+                        userIpParam, myWordParam, datetimeParam, anagramIdParam);
+                }
+            }
         }
 
         public IList<UserModel> GetUserLogFromDB()
         {
             List<UserModel> users = new();
-            
-            var logs = db.UserLog.Include(b => b.Word)
-                    .ToList();
 
-            foreach (var log in logs)
+            using (var db = new AnagramDBCodeFirstContext())
             {
-                users.Add(new UserModel(log.UserLogID, log.Ip,
-                    log.SearchingWord, log.SearchTime,
-                    log.Word.Word));
+                var logs = db.UserLog.Include(b => b.Word)
+                        .ToList();
+
+                foreach (var log in logs)
+                {
+                    users.Add(new UserModel(log.UserLogID, log.Ip,
+                        log.SearchingWord, log.SearchTime,
+                        log.Word.Word));
+                }
             }
-            
+
             return users;
         }
     }
--- AnagramSolver.BusinessLogic/DBWordRepositoryDatabaseFirst.cs
+++ AnagramSolver.BusinessLogic/DBWordRepositoryDatabaseFirst.cs
@@ -9,21 +9,22 @@ using System.Linq;
 namespace AnagramSolver.BusinessLogic
 {
     public class DBWordRepositoryDatabaseFirst : IWordRepository
-    {
-        private readonly AnagramDBContext db;
+    {       
         public DBWordRepositoryDatabaseFirst()
-        {
-            db = new AnagramDBContext();
+        {            
         }
 
         public IList<WordModel> GetWords()
         {
             IList<WordModel> words = new List<WordModel>();
-                    
-            foreach(var word in db.Words.ToList())
+            
+            using (var db = new AnagramDBContext())
             {
-                words.Add(new WordModel(word.Id, word.Word1, word.Category));
-            }         
+                foreach(var word in db.Words.ToList())
+                {
+                    words.Add(new WordModel(word.Id, word.Word1, word.Category));
+                }
+            }
 
             return words;            
         }
@@ -31,15 +32,18 @@ namespace AnagramSolver.BusinessLogic
         public IList<WordModel> SearchWords(string myWord)
         {
             List<WordModel> words = new List<WordModel>();
-                                                     
-            var wordsFound = from s in db.Words 
-                        where s.Word1.Contains(myWord) 
-                        select s;
+                            
+            using (var db = new AnagramDBContext())
+            {                
+                var wordsFound = from s in db.Words 
+                            where s.Word1.Contains(myWord) 
+                            select s;
 
-            foreach (var word in wordsFound)
-            {
-                words.Add(new WordModel(word.Id, word.Word1, word.Category));
-            }                  
+                foreach (var word in wordsFound)
+                {
+                    words.Add(new WordModel(word.Id, word.Word1, word.Category));
+                }
+            }         
 
             return words;           
         }
@@ -53,20 +57,25 @@ namespace AnagramSolver.BusinessLogic
                     SearchingWord = myWord,
                     AnagramId = ana.Id,                        
                 };
-                
-                db.Add(cachedWord);                                                   
-            }
-            db.SaveChanges();
+                using(var db = new AnagramDBContext())
+                {
+                    db.Add(cachedWord);
+                    db.SaveChanges();
+                }                
+            }           
         }
 
         public bool CheckCachedWord(string myWord)
         {           
             bool isWordExist = false;
             int count = 0;
-            
-            var myWordParam = new SqlParameter("myWord", myWord);
 
-            count = db.CachedWords.Count(t => t.SearchingWord == myWord);                      
+            using (var db = new AnagramDBContext())
+            {
+                var myWordParam = new SqlParameter("myWord", myWord);
+
+                count = db.CachedWords.Count(t => t.SearchingWord == myWord);           
+            }
 
             isWordExist = count > 0 ? true : false;
 
@@ -75,54 +84,66 @@ namespace AnagramSolver.BusinessLogic
 
         public IList<WordModel> GetWordFromCache(string myWord)
         {
-            List<WordModel> anagrams = new();            
+            List<WordModel> anagrams = new();
             
-            var query = from s in db.CachedWords
-                        join sa in db.Words on s.AnagramId equals sa.Id
-                        where s.SearchingWord == myWord
-                        select sa;
-
-            foreach(var word in query)
+            using(var db = new AnagramDBContext())
             {
-                anagrams.Add(new WordModel(word.Id, word.Word1, word.Category));
-            }                        
+                var query = from s in db.CachedWords
+                            join sa in db.Words on s.AnagramId equals sa.Id
+                            where s.SearchingWord == myWord
+                            select sa;
+
+                foreach(var word in query)
+                {
+                    anagrams.Add(new WordModel(word.Id, word.Word1, word.Category));
+                }
+            }              
 
             return anagrams;          
         }
 
         public void DeleteRecordFromWordTable(string myWord)
-        {            
-            var myWordParam = new SqlParameter("target", myWord);
-            db.Database.ExecuteSqlRaw("DeleteRecordFromWord @target", myWordParam);                       
+        {        
+            using(var db = new AnagramDBContext())
+            {
+                var myWordParam = new SqlParameter("target", myWord);
+                db.Database.ExecuteSqlRaw("DeleteRecordFromWord @target", myWordParam);
+            }            
         }
 
         public void InsertUserLogToDB(IList<WordModel> words, string myWord, string userIp)
-        {                        
-            foreach (WordModel ana in words)
+        {            
+            using (var db = new AnagramDBContext())
             {
-                var userIpParam = new SqlParameter("@userIp", userIp);
-                var myWordParam = new SqlParameter("@searchingWord", myWord);
-                var datetimeParam = new SqlParameter("@searchTime", System.DateTime.Now.ToString());
-                var anagramIdParam = new SqlParameter("@foundAnagramId", ana.Id);
+                foreach (WordModel ana in words)
+                {
+                    var userIpParam = new SqlParameter("@userIp", userIp);
+                    var myWordParam = new SqlParameter("@searchingWord", myWord);
+                    var datetimeParam = new SqlParameter("@searchTime", System.DateTime.Now.ToString());
+                    var anagramIdParam = new SqlParameter("@foundAnagramId", ana.Id);
 
-                db.Database.ExecuteSqlRaw("InsertUserLog @userIp, @searchingWord, " +
-                    "@searchTime, @foundAnagramId",
-                    userIpParam, myWordParam, datetimeParam, anagramIdParam);
-            }                              
+                    db.Database.ExecuteSqlRaw("InsertUserLog @userIp, @searchingWord, " +
+                        "@searchTime, @foundAnagramId",
+                        userIpParam, myWordParam, datetimeParam, anagramIdParam);
+                }
+            }                   
         }
 
         public IList<UserModel> GetUserLogFromDB()
         {
             List<UserModel> users = new();
-                
-            var logs = db.UserLogs.Include(b => b.FoundAnagram).ToList();
+    
+            using(var db = new AnagramDBContext())
+            {
+                var logs = db.UserLogs.Include(b => b.FoundAnagram).ToList();
 
-            foreach (var log in logs)
-            {              
-                users.Add(new UserModel(log.UserId, log.Ip, 
-                    log.SearchingWord, log.SearchTime, 
-                    log.FoundAnagram.Word1));
-            }           
+                foreach (var log in logs)
+                {              
+                    users.Add(new UserModel(log.UserId, log.Ip, 
+                        log.SearchingWord, log.SearchTime, 
+                        log.FoundAnagram.Word1));
+                }
+            }
                 
             return users;           
         }        
